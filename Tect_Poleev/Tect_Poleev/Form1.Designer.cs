7b6a4639656caa6f4da897f32c509f09ba2c6d26
miroNever <123352959+miroNever@users.noreply.github.com>
2023-05-24 12:39:07 +0500

    Add files via upload

---
+++ Tect_Poleev/Tect_Poleev/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Tect_Poleev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите слово которое\r\nхотите найти в файле";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Искать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Задание 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(540, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Задание 2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(543, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Искать частоту чисел";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(543, 129);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(151, 134);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Текст в файле";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(76, 188);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(292, 108);
+            this.listBox2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(543, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Искать число * частота";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 480);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Tect_Poleev/Tect_Poleev/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tect_Poleev
+{
+    public partial class Form1 :Form
+    {
+        public Form1 ()
+        {
+            InitializeComponent( );
+        }
+        static bool Check (string word)
+        {
+            bool check = false;
+            word = word.ToLower( );
+            for ( int i = 0; i < word.Length; i++ )
+            {
+                char c = word [ i ];
+                if ( c >= 'а' && c <= 'я' )
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                    break;
+                }
+            }
+            return check;
+        }
+        static string ReadingFile (string line)
+        {
+            StreamReader sr = File.OpenText("file.txt");
+            while ( !sr.EndOfStream )
+            {
+                line = sr.ReadLine( );
+            }
+            sr.Close( );
+            return line;
+        }
+        static int Search (string word, string line)
+        {
+            List<string> list = line.Split(' ').ToList( );
+            IEnumerable<string> result =
+                //берём w из листа
+                from w in list
+                //ищем совпадение слова с словом из файла
+                where w.Contains(word)
+                select w;
+            int count = 0;
+            foreach ( string w in result )
+            {
+                count++;
+            }
+            return count;
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            if ( textBox1.TextLength != 0 )
+            {
+                string word = textBox1.Text;
+                //проверка на русские буквы
+                if ( Check(word) )
+                {
+                    string line = "";
+                    //чтение из файла
+                    line = ReadingFile(line);
+                    int count = Search(word.ToLower( ), line.ToLower( ));
+                    if ( count > 0 )
+                        MessageBox.Show($"Найдены {count} вхождения(ий) поискового запроса {word}");
+                    else
+                        MessageBox.Show($"Не найдены входления(ий) поискового запроса {word}");
+                }
+                else
+                {
+                    MessageBox.Show("Слово должно содержать только русские буквы!");
+                }
+            }
+            else
+                MessageBox.Show("Поле пустое");
+        }
+
+        double [ ] number = { 5.1, 1.3, 9.2, 2.3, 5.1, 3, 2.3, 1.3, 4.4 };
+        private void button2_Click (object sender, EventArgs e)
+        {
+            listBox1.Items.Clear( );
+            var result = number.GroupBy(x => x)//групируем числа из массива без повторов
+                .Select(x => new { Number = x.Key, Frequency = x.Count( ) }) //преобразуем в обьект с двумя свойствами:
+                                                                             //Number значение, Frequency частота
+                .OrderBy(x => x.Number); // сортируем обьект по возростанию
+            listBox1.Items.Add("Числа - Частота");
+            foreach ( var x in result )
+            {
+                string item = $"{x.Number} - {x.Frequency}";
+                listBox1.Items.Add(item);
+            }
+        }
+        private void button3_Click (object sender, EventArgs e)
+        {
+            listBox1.Items.Clear( );
+            var result = number.GroupBy(x => x)//групируем числа из массива без повторов
+                .Select(x => new { Number = x.Key, Frequency = x.Count( ) })//преобразуем в обьект с двумя свойствами:
+                                                                            //Number значение, Frequency частота
+                .OrderBy(x => x.Number); // сортируем обьект по возростанию
+            listBox1.Items.Add("Число * частоту - Частота");
+            foreach ( var x in result )
+            {
+                string item = $"{x.Number * x.Frequency} - {x.Frequency}";
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void Form1_Load (object sender, EventArgs e)
+        {
+            //загрузка текста из файла в ListBox
+            string line = "";
+            line = ReadingFile(line);
+            listBox2.Items.Add(line);
+
+            //загрузка массива чисел в ListBox
+            foreach ( double num in number)
+            {
+                listBox1.Items.Add(num.ToString( ));
+            }
+        }
+
+    }
+}
